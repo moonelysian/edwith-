015c15a75cb1e8529bdd5b77c906709f33877f8c
moonelysian <dandyblack@naver.com>
2019-08-28 14:33:26 +0900

    DI 확인하기

---
+++ mavenweb/diexam01/src/main/java/kr/or/connect/diexam01/ApplicationContextExam02.java
@@ -0,0 +1,16 @@
+package kr.or.connect.diexam01;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class ApplicationContextExam02 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ApplicationContext ac = new ClassPathXmlApplicationContext("applicationContext.xml");
+		
+		Car car = (Car)ac.getBean("c");
+		car.run();
+	}
+
+}
